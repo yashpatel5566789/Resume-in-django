9085800b7dbdf29388cba1a40975637a8b37c725
yashpatel5566789 <yashpatel5566789@gmail.com>
2020-01-21 22:39:26 +0530

    staticfile updated

--- dprojx/settings.py
+++ dprojx/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'templates',
     
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles_')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
