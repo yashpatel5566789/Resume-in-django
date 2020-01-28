8f04de79dae89710f4a58fe5cc5288a5271a3f37
yashpatel5566789 <yashpatel5566789@gmail.com>
2020-01-28 14:34:49 +0530

    deployed editing

--- dprojx/settings.py
+++ dprojx/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -140,3 +140,4 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 #django_heroku.settings(locals())
+django_heroku.settings(locals())
